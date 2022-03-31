1021f448567ace53d027e4add99bf389b34e4462
Dayanna Martínez <101811952+Dayanna-Martinez-Arus@users.noreply.github.com>
2022-03-31 11:22:34 -0500

    sesion 5 con la tarea promedio

--- sesion5/app/funciones2.js
+++ sesion5/app/funciones2.js
@@ -1,13 +1,33 @@
 const { default: axios } = require("axios")
 
-const poblacion =() => async() => {
-const{data :{data}}= await    axios.get('apihttps://datausa.io/api/data?drilldowns=Nation&measures=Population');
+const poblacion =async()  => {
+const{data :{data}}= await axios.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
 
 let total = 0;
-
+//map tambien sirve para hacer ciclos fáciles y rápidos
 data.map(x=> {
     total =total + x.Population;
 });
-console.log(total);
+console.log('la cantidad de poblacion es:'+total);
+
+const cantidad = 14;
+data.map(x=> {
+    promedio =total / cantidad;
+});
+console.log('el promedio es:'+promedio);
 }
 poblacion();
+
+//promedio
+/*
+let promedio;
+const cantidad = 14;
+data.map(x=> {
+    promedio = total / cantidad;
+});
+console.log('el promedio es:'+promedio);
+poblacion();
+
+
+
+*/
